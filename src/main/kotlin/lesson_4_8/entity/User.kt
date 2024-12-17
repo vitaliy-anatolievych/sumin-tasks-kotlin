316da827c2d4e1d001aab5b1463235762bac608b
nivinskiyav <nivinskiyav@gmail.com>
2024-12-17 06:30:16 +0200

    feat: add field maxBooks and validation for this field

--- src/main/kotlin/lesson_4_8/entity/User.kt
+++ src/main/kotlin/lesson_4_8/entity/User.kt
@@ -11,18 +11,26 @@ package lesson_4_8.entity
 data class User(
     val id: Int,
     val name: String,
-    private val _borrowedBooks: MutableList<Book> = mutableListOf()
+    private val _borrowedBooks: MutableList<Book> = mutableListOf(),
+    private val maxBooks: Int = 5
 ) {
     init {
         require(id >= 0) { "ID користувача не може бути від'ємним" }
         require(name.isNotBlank()) { "Ім'я користувача не може бути пустим" }
+        require(maxBooks > 0) { "Максимальна кількість книг має бути більше 0" }
     }
 
     val borrowedBooks: List<Book>
         get() = _borrowedBooks.toList()
 
-    internal fun addBook(book: Book) {
-        _borrowedBooks.add(book)
+    internal fun addBook(book: Book): Boolean {
+        if (_borrowedBooks.size >= maxBooks) {
+            return false
+        }
+        if (_borrowedBooks.contains(book)) {
+            return false
+        }
+        return _borrowedBooks.add(book)
     }
 
     internal fun removeBook(book: Book) {
